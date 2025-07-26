f7b7b50657f60a31505b5b8363975699794e6a4a
MrVyde <Mrvyde@outlook.com>
2025-07-26 08:57:56 +0100

    Generate grid squares dynamically and append to container div

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("#container")
+for (let i = 0; i < 256; i++) {
+    const cell = document.createElement("div")
+    cell.classList.add("cell")
+    container.appendChild(cell)
+}
+
+
+
+
